b7e7e1567eabddff7ff2f627bd3536eb8507ab8f
Daniel Escamilla <descamilla128@gmail.com>
2022-01-11 09:01:45 -0500

    final commit

--- veryHard/script.js
+++ veryHard/script.js
@@ -1,6 +1,21 @@
-function veryHard() {
-    let userText = prompt("What is 2 * 2?");
-    if (userText === "") {
-      document.getElementById("Anwser").innerHTML = "No Response";
-      console.log('No Response');
-    }
+function veryhard() {
+  num1 = Number(prompt("First Number?"));
+  numOpertor= prompt("Enter a math operation +, -, *, or /");
+  num2 = Number(prompt("Second Number?"));
+
+  if (numOpertor === "+") {
+      add();
+  }
+  else if (numOpertor === "-") {
+      subtract();
+  }
+  else if (numOpertor === "*") {
+      multiply();
+  }
+  else if (numOpertor === "/") {
+      divide ();
+  }
+  else {
+      console.log(`Invlaid math opertor used`);
+  }
+}
